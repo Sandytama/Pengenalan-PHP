98297d976d788f3d06871976da003fd72028e65c
Sandytama <114657176+Sandytama@users.noreply.github.com>
2022-09-29 21:05:23 +0700

    Add files via upload

---
+++ Tgs Framework.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+        <head>
+            <title> Playlist </title>
+            <style>
+                
+                .container{
+                    height: 370px;
+                    width: 420px;
+                    float:left;
+                    background-color:rgb(33, 36, 38) ;
+                    color:yellow;
+                    margin-inline-start: 110px;
+                    
+                    
+                }
+                .list{
+                
+                    height : 100px;
+                    width: 500px;
+        
+                    
+                }
+               .image{
+                   float:left;
+                   margin:10px 80px;
+                   padding:10px;
+                
+                }
+                .text{
+                    font-family: Times New Roman;
+        
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="title"><justify><p> </justify></p></div>
+                    <div class="image">
+                        <center><img class="img" src="poco.png" widht="150px" height="150px"></center>
+                        <div class="text"><justify>Nama barang : Smartphone</justify></div>
+                        <div class="text"><justify>Kode barang : Poco<justify></div>
+                        <div class="text"><justify>Spesifikasi barang : Snapdragon 870</justify></div>
+                        <div class="text"><justify>Harga barang : Rp. 4,999.000</justify></div>
+                        <div class="text"><justify>Ekspedisi pengiriman barang : 3 hari</justify></div>
+                    </div>
+                
+               
+        </body>
+</html>
